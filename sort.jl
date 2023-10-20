1a38b0914f114cf7545415901f06225aadba8729
Ivkosss <148562325+Ivkosss@users.noreply.github.com>
2023-10-20 16:40:45 +0200

    Update sort.jl

--- sort.jl
+++ sort.jl
@@ -2,7 +2,7 @@
 # If we want sort a column upto a certain number
 B = 3
 t = sort(df1.z; alg = PartialQuickSort(B))
- 
+#izmenaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 # passing the t variable in the dataframe
 df1.z = t 
 df1
